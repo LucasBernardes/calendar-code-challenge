645ee831a9c3d349b1e2314f856cb3f0246b887d
Lucas <lukazbernardez@gmail.com>
2020-11-27 00:35:53 -0300

    working month

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,16 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import './styles.css';
+import { fireEvent } from '@testing-library/react';
 
 function Calendar() {
-  const [monthOffset, setMonthOffset] = useState(0)
+  const [monthOffset, setMonthOffset] = useState(1)
   const currentMonth = moment().add(monthOffset, 'months');
   const lastMonth = moment().add(monthOffset - 1, 'months');
   const nextMonth = moment().add(monthOffset + 1, 'months');
   // const currentMonthDates = new Array(currentMonth.daysInMonth()).fill(null).map((x, i) => currentMonth.startOf('month').add(i, 'days'));
-  const currentMonthDates = new Array(moment().daysInMonth()).fill(null).map((x, i) => moment().startOf('month').add(i, 'days'));
+  let currentMonthDates = Array.from({length: moment().add(1,'months').daysInMonth()}, (x, i) => moment().add(1,'months').startOf('month').add(i, 'days'));
   const weekArray = moment.weekdays()
+
+  const getDaysByMonth = (month) => {
+    console.log('month', month)
+    const daysInMonth = moment(month).daysInMonth();
+    return Array.from({length: daysInMonth}, (v, k) => k + 1)
+  };
+
+  console.log('mudou currentMonthDates', currentMonthDates[0].day())
+  const date = currentMonthDates[0].day();
+  if(date > 0) {
+    for (var i = 0; i < date; i++) {
+      currentMonthDates.unshift(moment().add(i + 1, 'months'))
+    }
+  }
+  while(currentMonthDates.length < 35) {
+    currentMonthDates.push(moment())
+  }
+  console.log(currentMonthDates)
+
   
+
   const events = [
     {
       name: 'Consulta médico',
@@ -26,11 +47,11 @@ function Calendar() {
     },
     {
       name: 'Entregar Trabalho',
-      hour: '17:00',
+      hour: '17:20',
     },
     {
       name: 'Consulta médico',
-      hour: '13:00',
+      hour: '13:10',
     },
     // {
     //   name: 'Lavar carro',
@@ -52,12 +73,12 @@ function Calendar() {
         {
           events.map((event) => {
             return (
-              <div className="calendar-event-item">
-                <span class="event-circle"></span>
+              <div key={event.hour} className="calendar-event-item">
+                <span className="event-circle"></span>
                 <span className="event-title">
-                  {event.name}
+                  {event.name && event.name}
                 </span>
-                <span class="event-hour">{event.hour}</span>
+                <span className="event-hour">{event.hour && event.hour}</span>
               </div>
             );
           })
@@ -95,18 +116,17 @@ function Calendar() {
       </div>
     );
   }
-
+  console.log(currentMonthDates)
   return (
     <>
       <MonthChooserHeader />
       <HeaderCalendar />
       <div className="calendar-wrapper">
-        
         {
           currentMonthDates.map((date) => {
-            console.log(date.day())
+            console.log(date)
             return (
-              <div className="calendar-item-wrapper" key={date.date()}>
+              <div className="calendar-item-wrapper" key={date.format('L')}>
                 <div className={`${(date.day() === 0 || date.day() === 6) ? 'weekend' : 'calendar-item'}`}>
                   <div className="event-date-number">
                     {date.date()}
